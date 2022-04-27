a219a6b1cb0fb0ba4caa0f902811b334fd009060
halco102 <ado.halilovic@outlook.com>
2022-04-27 17:18:08 +0200

    deleted unused import

--- src/main/java/com/project/reddit/repository/PostRepository.java
+++ src/main/java/com/project/reddit/repository/PostRepository.java
@@ -1,14 +1,11 @@
 package com.project.reddit.repository;
 
-import com.project.reddit.dto.post.PostDto;
-import com.project.reddit.dto.projection.TestProjection;
 import com.project.reddit.model.content.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Set;
 
 @Repository
