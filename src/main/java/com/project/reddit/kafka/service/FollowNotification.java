0433c91cd67ca1d7b3927912e2d2a264bc1c095b
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-10-15 18:30:43 +0200

    deleting unused classes

--- src/main/java/com/project/reddit/kafka/service/FollowNotification.java
+++
@@ -1,9 +0,0 @@
-package com.project.reddit.kafka.service;
-
-import com.project.reddit.kafka.model.Notification;
-
-public interface FollowNotification {
-
-    void sendNotificationToFollowers(Notification user, String topic);
-
-}
