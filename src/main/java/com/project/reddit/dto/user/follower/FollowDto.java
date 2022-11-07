b7f5b83bd0d4a51d829e0aab70378b91f77fd5ad
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-11-07 09:47:25 +0100

    deleting EqualsAndHashCode

--- src/main/java/com/project/reddit/dto/user/follower/FollowDto.java
+++ src/main/java/com/project/reddit/dto/user/follower/FollowDto.java
@@ -5,14 +5,14 @@ import lombok.*;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
 public class FollowDto extends UserAbstract {
 
     public FollowDto(Long id, String username, String email, LocalDate createdAt, String imageUrl) {
         super(id, username, email, createdAt, imageUrl);
     }
+
 
 
 }
